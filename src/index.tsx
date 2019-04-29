b3f67dd1b24f4e2e70fe2590b8af62e529db42c4
Casey Primozic <me@ameo.link>
2019-04-29 02:08:25 -0700

    Initial commit
     * Set up project boilerplate including config files for Babel, NPM, Webpack, TypeScript, ESLint, and Prettier
     * Create `LICENSE` file with the Blue Oak license

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <span>Hello, World!</span>;
+
+ReactDOM.render(<App />, document.getElementById('root')!);
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(tsx?)|(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.hbs$/,
+        use: 'handlebars-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.tsx', '.js', '.jsx', '.wasm'],
+    modules: [path.resolve('./node_modules'), path.resolve('.')],
+    alias: {
+      Tone: path.resolve('./node_modules/tone/Tone'),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      alwaysWriteToDisk: true,
+      title: 'Personal Spotify Stats',
+      minify: true,
+      template: 'index.hbs',
+    }),
+  ],
+};
