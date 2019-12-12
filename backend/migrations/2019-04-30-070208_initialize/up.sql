1d3ee8f290ed43528a77d4c8a36906e22ac797a2
Casey Primozic <me@ameo.link>
2019-12-12 14:15:02 -0800

    Update database migration script
     * Use compressed row format on the big update tables to improve their size
     * Use ASCII charset for a couple of tables that don't need unicode

--- backend/migrations/2019-04-30-070208_initialize/up.sql
+++ backend/migrations/2019-04-30-070208_initialize/up.sql
@@ -3,7 +3,7 @@ START TRANSACTION;
     `id` BIGINT NOT NULL AUTO_INCREMENT,
     `creation_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `last_update_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `spotify_id` VARCHAR(255) UNIQUE NOT NULL,
+    `spotify_id` VARCHAR(191) UNIQUE NOT NULL,
     `username` TEXT NOT NULL,
     `token` TEXT NOT NULL,
     `refresh_token` TEXT NOT NULL,
@@ -15,7 +15,7 @@ START TRANSACTION;
   -- Table for mapping between spotify IDs and internal IDs
   CREATE TABLE `spotify_homepage`.`spotify_items` (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `spotify_id` VARCHAR(255) UNIQUE NOT NULL,
+    `spotify_id` VARCHAR(191) UNIQUE NOT NULL,
     PRIMARY KEY (`id`)
   );
   CREATE INDEX spotify_id_ix ON `spotify_homepage`.`spotify_items` (spotify_id);
@@ -31,23 +31,16 @@ START TRANSACTION;
   );
   ALTER TABLE `spotify_homepage`.`tracks_artists` ADD UNIQUE `unique_index`(`track_id`, `artist_id`);
 
-  CREATE TABLE `spotify_homepage`.`spotify_items` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `spotify_id` VARCHAR(255) UNIQUE NOT NULL,
-    PRIMARY KEY (`id`)
-  );
-  CREATE INDEX spotify_id_ix ON `spotify_homepage`.`spotify_items` (spotify_id);
-
   CREATE TABLE `spotify_homepage`.`track_rank_snapshots` (
     `id` BIGINT NOT NULL AUTO_INCREMENT,
     `user_id` BIGINT NOT NULL,
     `update_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `spotify_id` VARCHAR(255) NOT NULL,
+    `spotify_id` VARCHAR(191) NOT NULL,
     `timeframe` TINYINT UNSIGNED NOT NULL,
     `ranking` SMALLINT UNSIGNED NOT NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-  );
+  ) ROW_FORMAT=COMPRESSED CHARSET=ascii;
   CREATE INDEX user_id_ix ON `spotify_homepage`.`track_rank_snapshots` (user_id);
   CREATE INDEX update_time_ix ON `spotify_homepage`.`track_rank_snapshots` (update_time);
 
@@ -55,12 +48,12 @@ START TRANSACTION;
     `id` BIGINT NOT NULL AUTO_INCREMENT,
     `user_id` BIGINT NOT NULL,
     `update_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `spotify_id` VARCHAR(255) NOT NULL,
+    `spotify_id` VARCHAR(191) NOT NULL,
     `timeframe` TINYINT UNSIGNED NOT NULL,
     `ranking` SMALLINT UNSIGNED NOT NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-  );
+  ) ROW_FORMAT=COMPRESSED CHARSET=ascii;
   CREATE INDEX user_id_ix ON `spotify_homepage`.`artist_rank_snapshots` (user_id);
   CREATE INDEX update_time_ix ON `spotify_homepage`.`artist_rank_snapshots` (update_time);
 COMMIT;
