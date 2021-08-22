acd93074cdde69ae451011e6115eace71e9bbd92
Casey Primozic <me@ameo.link>
2021-08-21 22:26:55 -0700

    Huge progress on artist map
     * So much stuff.  Labels render dynamically and de-render when player moves away, popularity is included in the packed binary embedding and artist geometries are scaled to match.
     * Tuned network rendering + transparency of a bunch of stuff as well

--- backend/src/artist_embedding/map_3d.rs
+++ backend/src/artist_embedding/map_3d.rs
@@ -1,6 +1,14 @@
-use tokio::sync::OnceCell;
+use std::convert::TryInto;
 
-use crate::artist_embedding::{parse_positions, ArtistEmbeddingContext};
+use fnv::FnvHashMap as HashMap;
+use tokio::{sync::OnceCell, task::spawn_blocking};
+
+use crate::{
+    artist_embedding::{parse_positions, ArtistEmbeddingContext},
+    db_util::{get_artist_spotify_ids_by_internal_id, retrieve_mapped_spotify_ids},
+    spotify_api::fetch_artists,
+    DbConn,
+};
 
 // const PACKED_3D_ARTIST_COORDS_URL: &str = "https://ameo.dev/artist_map_embedding_3d.w2v";
 const PACKED_3D_ARTIST_COORDS_URL: &str = "https://ameo.dev/pca.w2v";
@@ -25,13 +33,83 @@ async fn build_3d_artist_map_ctx() -> ArtistEmbeddingContext<3> {
 static MAP_3D_ARTIST_CTX: OnceCell<ArtistEmbeddingContext<3>> = OnceCell::const_new();
 static PACKED_3D_ARTIST_EMBEDDING: OnceCell<Vec<u8>> = OnceCell::const_new();
 
-async fn build_packed_3d_artist_coords() -> Vec<u8> {
-    let map_ctx_3d = MAP_3D_ARTIST_CTX.get_or_init(build_3d_artist_map_ctx).await;
-    map_ctx_3d.serialize_to_packed_binary()
+async fn get_all_artist_popularities_by_id(
+    spotify_access_token: &str,
+    all_artist_spotify_ids: Vec<String>,
+) -> Result<HashMap<String, u8>, String> {
+    let all_artist_spotify_ids: Vec<&str> = all_artist_spotify_ids
+        .iter()
+        .map(|id| id.as_str())
+        .collect();
+    let all_artists = fetch_artists(spotify_access_token, &all_artist_spotify_ids).await?;
+    let mut artist_popularities_by_id: HashMap<String, u8> = HashMap::default();
+    for artist in all_artists {
+        artist_popularities_by_id.insert(
+            artist.id,
+            artist
+                .popularity
+                .map(|pop| pop.try_into().unwrap())
+                .unwrap_or(10),
+        );
+    }
+    Ok(artist_popularities_by_id)
 }
 
-pub async fn get_packed_3d_artist_coords() -> &'static [u8] {
-    PACKED_3D_ARTIST_EMBEDDING
-        .get_or_init(build_packed_3d_artist_coords)
+async fn build_packed_3d_artist_coords(
+    conn: &DbConn,
+    spotify_access_token: &str,
+) -> Result<Vec<u8>, String> {
+    let map_ctx_3d = MAP_3D_ARTIST_CTX.get_or_init(build_3d_artist_map_ctx).await;
+
+    let all_artist_internal_ids: Vec<i32> = map_ctx_3d
+        .artist_position_by_id
+        .keys()
+        .map(|key| (*key) as i32)
+        .collect();
+    let artist_spotify_ids_by_internal_id: HashMap<i32, String> =
+        get_artist_spotify_ids_by_internal_id(conn, all_artist_internal_ids)
+            .await
+            .unwrap();
+    let artist_spotify_ids: Vec<String> = artist_spotify_ids_by_internal_id
+        .values()
+        .map(|id| id.to_string())
+        .collect();
+    let popularities = get_all_artist_popularities_by_id(spotify_access_token, artist_spotify_ids)
         .await
+        .unwrap();
+    info!("Fetched {} popularities", popularities.len());
+
+    let internal_ids = retrieve_mapped_spotify_ids(conn, popularities.keys()).await?;
+    let mut popularities_by_internal_id: HashMap<i32, u8> = HashMap::default();
+    for (spotify_id, popularity) in popularities {
+        let internal_id = match internal_ids.get(&spotify_id) {
+            Some(id) => *id,
+            None => continue,
+        };
+
+        popularities_by_internal_id.insert(internal_id, popularity);
+    }
+
+    Ok(map_ctx_3d.serialize_to_packed_binary(Some(popularities_by_internal_id)))
+}
+
+pub async fn get_packed_3d_artist_coords(
+    conn: &DbConn,
+    spotify_access_token: &str,
+) -> Result<&'static [u8], String> {
+    PACKED_3D_ARTIST_EMBEDDING
+        .get_or_try_init(|| async {
+            let pre_saved_map = spawn_blocking(|| std::fs::read("packed_map_3d.bin").ok())
+                .await
+                .unwrap();
+            if let Some(map) = pre_saved_map {
+                info!("Found pre-saved 3d artist map, serving that...");
+                return Ok(map);
+            }
+
+            warn!("Pre-saved packed 3D artist map not found, generating...");
+            build_packed_3d_artist_coords(conn, spotify_access_token).await
+        })
+        .await
+        .map(|v| v.as_slice())
 }
--- frontend/src/artistAverager/ArtistMap/conf.ts
+++ frontend/src/artistAverager/ArtistMap/conf.ts
@@ -1,7 +1,14 @@
-export const BASE_ARTIST_COLOR = 0x1fcf36;
+export const BASE_ARTIST_COLOR = 0x22eecc;
 export const BASE_CONNECTION_COLOR = 0x2288ee;
 
 export const MOVEMENT_SPEED_UNITS_PER_SECOND = 48;
 
-export const ARTIST_LABEL_TEXT_SIZE = 144.8;
+export const ARTIST_LABEL_TEXT_SIZE = 44.8;
 export const ARTIST_GEOMETRY_SIZE = 1.7;
+
+export const SECONDS_BETWEEN_POSITION_UPDATES = 0.15;
+
+export const getArtistSize = (popularity: number): number => {
+  const x = popularity / 100;
+  return Math.pow(Math.pow(x, 2) * 10, 2.5) + 12 * Math.pow(x, 6) + 0.025 * x + 2;
+};
