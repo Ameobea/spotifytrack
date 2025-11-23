ed321e240639004ba691c099791b3f0edcf53143
Heneni <mark@heneniart.com>
2025-11-23 18:58:21 -0500

    Update webpack.prod.js to use configurable publicPath for Vercel

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,9 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require('./webpack.config');
 
+const siteUrlEnv = process.env.REACT_APP_SITE_URL || '';
+// Ensure trailing slash when an absolute URL is provided, otherwise use root '/'
+const publicPath = siteUrlEnv ? `${siteUrlEnv.replace(/\/$/, '')}/` : '/';
+
 module.exports = {
   ...config,
-  output: { ...config.output, publicPath: 'https://spotifytrack.b-cdn.net/' },
+  output: { ...config.output, publicPath },
   mode: 'production',
   devtool: 'source-map',
 };
